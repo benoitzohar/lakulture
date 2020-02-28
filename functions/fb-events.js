e901bcc2177018b1505edd89a506e3b18c346e77
Benoit Zohar <benoit.zohar@gmail.com>
2020-02-28 18:13:13 +0000

    wip add fb events

--- functions/fb-events.js
+++ functions/fb-events.js
@@ -4,26 +4,21 @@ const { FB_APP_ID, FB_APP_SECRET } = process.env;
 
 const fb = new Facebook({
   appId: FB_APP_ID,
-  appSecret: FB_APP_SECRET,
-  version: "v2.4"
+  appSecret: FB_APP_SECRET
 });
 fb.setAccessToken(`${FB_APP_ID}|${FB_APP_SECRET}`);
 
 exports.handler = async (event, context) => {
-  fb.api(
-    "/1645486232341067/events",
-    { fields: "cover,name,description,id,start_time" },
-    res => {
-      console.log("[BZ] res:", res);
+  fb.api("/1645486232341067/events", res => {
+    console.log("[BZ] res:", res);
 
-      if (!res || res.error) {
-        console.log(!res ? "error occurred" : res.error);
-        return;
-      }
-      console.log(res.id);
-      console.log(res.name);
+    if (!res || res.error) {
+      console.log(!res ? "error occurred" : res.error);
+      return;
     }
-  );
+    console.log(res.id);
+    console.log(res.name);
+  });
 };
 
-//exports.handler();
+exports.handler();
